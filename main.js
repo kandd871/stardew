f7ccdf067f5bd53f98e359ba43b00652af17249a
kandd871 <114494325+kandd871@users.noreply.github.com>
2024-05-27 16:47:39 +0530

    WERE BACKKK

--- main.js
+++ main.js
@@ -1,23 +1,46 @@
 const text = document.getElementById('text');
+const tester = document.querySelector('.tester')
 const slider = document.getElementById('slider');
 const colorPicker = document.getElementById('head');
 const colorPicker2 = document.getElementById('stroke');
 const colorPicker3 = document.getElementById('gradient');
 const body = document.querySelector('body');
+const stroke = document.getElementById('stroke')
+const fontsize = document.getElementById('fontsize');
+const mode = document.getElementById('mode');
+const modeButton = document.getElementById('mode-button');
+const info = document.querySelector('.info');
+const chars = document.querySelector('.chars')
+const about = document.getElementById('about-header');
+const subheaders = document.querySelectorAll('.subheader');
+const a = document.querySelector('a')
+const introtext = document.getElementById('intro-text');
 
-let currentFontSize = 10; // 10vw as the initial font size
+let currentFontSize = 112; // 10vw as the initial font size
 let fontSizeChanged = false; // Flag to track if font size has been changed
 
 function increaseFontSize() {
-  currentFontSize += 1;
-  text.style.fontSize = `${currentFontSize}vw`;
+  currentFontSize += 16;
+  text.style.fontSize = `${currentFontSize}px`;
   fontSizeChanged = true; // Update the flag when font size changes
+  fontsize.innerHTML = '';
+  fontsize.innerHTML = `${currentFontSize}px`;
 }
 
 function decreaseFontSize() {
-  currentFontSize -= 1;
-  text.style.fontSize = `${currentFontSize}vw`;
-  fontSizeChanged = true; // Update the flag when font size changes
+  if (currentFontSize == 0){
+    text.style.fontSize = `0px`;
+    fontsize.innerHTML = '';
+    fontsize.innerHTML = `${currentFontSize}px`;
+    currentFontSize -= 0;
+  } else {
+    currentFontSize -= 16;
+    text.style.fontSize = `${currentFontSize}px`;
+    fontSizeChanged = true; // Update the flag when font size changes
+    fontsize.innerHTML = '';
+    fontsize.innerHTML = `${currentFontSize}px`;
+  }
+ 
 }
 
 document.getElementById('increaseFontSize').addEventListener('click', increaseFontSize);
@@ -84,8 +107,44 @@ function watchColorPicker2(event) {
 colorPicker3.addEventListener("input", watchColorPicker3, false);
 
 function watchColorPicker3(event) {
-  body.style.background = `linear-gradient(white, ${event.target.value})`;
+  document.documentElement.style.setProperty('--background', event.target.value);
+  document.documentElement.style.setProperty('--background-rgb', hexToRgb(event.target.value));
+  tester.style.background = `linear-gradient(white, ${event.target.value})`;
+  // slider.style.boxShadow = `0px 0px 5px 4px rgba(${hexToRgb(event.target.value)}, .5)`;
+
 }
+
+
+mode.addEventListener("click", function(){
+  info.classList.toggle('dark-modeText');
+  chars.classList.toggle('dark-modeChars');
+  about.classList.toggle("dark-modeHeader");
+  mode.classList.toggle("dark-modeButton");
+  subheaders.forEach(function(subheader){
+  subheader.classList.toggle("dark-modeSubheader")
+  })
+
+  if (modeButton.textContent === "● DARK MODE") {
+    modeButton.textContent = "● LIGHT MODE";
+    body.style.cursor = `url('cursor2.png') 10 10, auto`;
+    introtext.innerHTML = '';
+    introtext.innerHTML = `
+    <p id="intro-text" class="arial">
+          The typeface was born from a curiosity about combining traditional design with technology, aiming to create an interplay between pixel and serif typography. Interested in how a grid and symbol could reimagine the nature of serifs, Stardew uses a flower-like pixel (<span class="pixel font"><img src="star3.png"></span>) to form its characters. The placement and varying repetition of this symbol on a grid reinforce the stress of serif typefaces. A different variation of the pixel (<span class="pixel font"><img src="star4.png"></span>) is used for the serifs, dots, and other aspects, creating a playful distinction. The serifs are also tapered, and the letters have a slightly tilted axis, resembling old-style serifs.
+        </p>
+    `
+  } else if (modeButton.textContent === "● LIGHT MODE"){
+    modeButton.textContent = "● DARK MODE";
+    body.style.cursor = `url('cursor.png') 10 10, auto`;
+    introtext.innerHTML = '';
+    introtext.innerHTML = `
+    <p id="intro-text" class="arial">
+    The typeface was born from a curiosity about combining traditional design with technology, aiming to create an interplay between pixel and serif typography. Interested in how a grid and symbol could reimagine the nature of serifs, Stardew uses a flower-like pixel (<span class="pixel font"><img src="star1.png"></span>) to form its characters. The placement and varying repetition of this symbol on a grid reinforce the stress of serif typefaces. A different variation of the pixel (<span class="pixel font"><img src="star.png"></span>) is used for the serifs, dots, and other aspects, creating a playful distinction. The serifs are also tapered, and the letters have a slightly tilted axis, resembling old-style serifs.
+    </p>
+    `
+  }
+})
+
 
 // Function to convert hex color code to RGB
 function hexToRgb(hex) {
@@ -100,7 +159,6 @@ function hexToRgb(hex) {
 
   return r + ',' + g + ',' + b;
 }
-
 
 
 
--- main1.js
+++ main1.js
@@ -1,7 +1,10 @@
 document.addEventListener('DOMContentLoaded', function () {
   const textElement = document.getElementById('text');
   const slider = document.getElementById('slider');
-  let weight = 40; // Default weight until the slider is used
+  let weight = 50; // Default weight until the slider is used
+  const fontsize = document.getElementById('fontsize');
+  const width = document.getElementById('widthmeter');
+  const weightmeter = document.getElementById('weightmeter')
 
   // Flag to track whether the cursor has been moved
   let cursorMoved = false;
@@ -10,8 +13,10 @@ document.addEventListener('DOMContentLoaded', function () {
   document.addEventListener('mousemove', function (e) {
     if (!cursorMoved) {
       // Initial font size change when the cursor is first moved
-      textElement.style.fontSize = '10vw';
-       textElement.style.marginTop = '30vh';
+      textElement.style.fontSize = '112px';
+      textElement.style.marginTop = '37vh';
+      fontsize.innerHTML = '';
+      fontsize.innerHTML = `128px`;
       cursorMoved = true;
     }
 
@@ -23,10 +28,38 @@ document.addEventListener('DOMContentLoaded', function () {
 
     // Update the font width and weight based on cursor position
     textElement.style.fontVariationSettings = `'wdth' ${fontWidth}, 'wght' ${weight}`;
+    width.innerHTML = '';
+    width.innerHTML = `WIDTH: ${fontWidth}`
+    weightmeter.innerHTML = '';
+    weightmeter.innerHTML = `WEIGHT: ${weight}`
+
   });
 
   // Event listener for slider input
   slider.addEventListener('input', function (e) {
     weight = e.target.value; // Update the weight variable
   });
+
+  // Handle placeholder visibility
+            textElement.addEventListener('focus', function () {
+                if (textElement.innerText.trim() === 'this is...\nstardew!\ntype away.') {
+                    textElement.innerText = '';
+                    // textElement.classList.remove('placeholder');
+                }
+            });
+
+            // textElement.addEventListener('blur', function () {
+            //     if (textElement.innerText.trim() === '') {
+            //         textElement.innerHTML = 'this is...<br>stardew!<br>type away.';
+            //         textElement.classList.add('placeholder');
+            //     }
+            // });
+
+            // textElement.addEventListener('input', function () {
+            //     if (textElement.innerText.trim() === '') {
+            //         textElement.classList.add('placeholder');
+            //     } else {
+            //         textElement.classList.remove('placeholder');
+            //     }
+            // });
 });
